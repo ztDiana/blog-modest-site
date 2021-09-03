a23cd36970a54ff97295dcf90e150116e0660b00
diana <dpidhaina@gmail.com>
2021-09-03 21:26:00 +0300

    slider

--- app.js
+++ app.js
@@ -1,3 +1,92 @@
+new Swiper('.swiper',{
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev'
+    },
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+
+        type:"bullets", 
+        // за замовчуванням
+        
+        320:{
+            dynamicBullets:false},
+        768:{
+            dynamicBullets:true,
+                },
+        992:{
+            dynamicBullets:true,
+            }
+        
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable:true,
+      },
+    //   можливість перетаскувати і свайпати слайди
+    simulateTouch:true,
+    // чутливість слайду
+    touchRatio:1,
+    // кут спрацювання свайпа
+    touchAngle:45,
+    // курсор свайпа
+    grabCursor:true,
+    // переключення при кліку на слайд
+    slideToClickedSlide: true,
+    hashNavigation:{
+        watchState:true,
+        // навигация с помощью стрелок браузера
+    },
+
+    keyboard:{
+        // on/off
+        enabled:true,
+        // включить. виключ только когда слайдер в пределах вьюпорта
+        onlyInViewport:true,
+        // включити виключити управління клавішами пейдж ап пейдж давн
+        pageUpDown:true,
+
+    },
+    
+    slidesPerView: 1, 
+    
+    spaceBetween:1,
+
+    // активний слайд по центру
+
+    centerSlide:true,
+    
+    mousewheel:{
+        sensitivity:1,
+        // класс обэкта на якому буде спрацьовувати прокрутка мишы
+        eventsTarget:".swiper"
+    },
+    // безкынечний слайдер
+    loop:true,
+    
+    // freemode-режим
+
+    freemode:true,
+
+    //  автопрокрутка
+     autoplay: {
+        // пауза мыж прокрутками
+        delay: 2000,
+        // Закінчити на останньому слайді
+        stopOnLastSlide: false,
+        // отключить после ручного переключения
+        disableOnInteraction: true,
+    },
+
+    speed:800,
+
+     effect:"fade",
+    fadeEffect:{
+        crossFade:true,
+    }
+});
+
 let btn_send_message=document.querySelector(".btn-send-message");
 console.log(btn_send_message)
 
